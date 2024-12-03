cd86cbf2449fd35e9653dadd296fb4816ef10a6d
Joshua Nanninga <jmarknanninga@gmail.com>
2024-12-03 15:52:46 -0600

    Added address list

---
+++ client/src/components/AddressList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { List, Label, Button } from 'semantic-ui-react';
+
+function AddressList({ addresses, markAsDelivered }) {
+  return (
+    <List divided relaxed>
+      {addresses.map((addr, index) => (
+        <List.Item key={addr.id}>
+          <List.Content floated='right'>
+            {addr.status === 'not_delivered' && (
+              <Button onClick={() => markAsDelivered(addr.id)} color='green' size='small'>
+                Delivered
+              </Button>
+            )}
+          </List.Content>
+          <List.Content>
+            <Label color={
+              addr.status === 'delivered' ? 'green' :
+              addr.status === 'left_notice' ? 'yellow' : 'red'
+            }>
+              {index + 1}
+            </Label>
+            {addr.address}
+          </List.Content>
+        </List.Item>
+      ))}
+    </List>
+  );
+}
+
+export default AddressList;
